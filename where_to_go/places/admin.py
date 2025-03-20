5d505d91a1ff59a0dd6a8b416c7d9ce57ac5bc82
svillors <tntzator9@gmail.com>
2025-03-20 22:39:12 +0600

    Add place model

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from places.models import Place
 
-# Register your models here.
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title',]
--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=100)
+    description_short = models.TextField()
+    description_long = models.TextField()
+    longitude = models.DecimalField(max_digits=17, decimal_places=14)
+    latitude = models.DecimalField(max_digits=17, decimal_places=14)
