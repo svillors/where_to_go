6eb5ab58985f4c81db11a766d1d0ffcabbaf3d4a
svillors <tntzator9@gmail.com>
2025-03-21 15:59:29 +0600

    Add model for images

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -7,3 +7,19 @@ class Place(models.Model):
     description_long = models.TextField()
     longitude = models.DecimalField(max_digits=17, decimal_places=14)
     latitude = models.DecimalField(max_digits=17, decimal_places=14)
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        related_name='images'
+    )
+    image = models.ImageField()
+    position = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f' Image number {self.position} for {self.place.title}'
