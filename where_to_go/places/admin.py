67fa7a4de74f33cd9fc672cb7b003f7f2e7bd789
svillors <tntzator9@gmail.com>
2025-03-23 17:09:49 +0600

    Add inline to admin page

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from places.models import Place, Image
 
 
+class ImagesInline(admin.TabularInline):
+    model = Image
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['id', 'title',]
+    inlines = [ImagesInline]
 
 
 @admin.register(Image)
