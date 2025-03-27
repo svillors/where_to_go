ace880e962547ef825d18d29d8321077b701623c
svillors <tntzator9@gmail.com>
2025-03-27 14:31:41 +0600

    Correct misstakes

--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -8,6 +8,6 @@ from places import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('places/<int:id>', views.places, name='places'),
+    path('places/<int:id>/', views.places, name='places'),
     path('tinymce/', include('tinymce.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
