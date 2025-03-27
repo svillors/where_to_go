ed4b1d7da3b11e5b8a525bfbd556e5f0e442e397
svillors <tntzator9@gmail.com>
2025-03-27 14:40:53 +0600

    Improve models

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -3,11 +3,11 @@ from tinymce import models as tinymce_models
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=100)
-    description_short = models.TextField()
-    description_long = tinymce_models.HTMLField()
-    longitude = models.DecimalField(max_digits=20, decimal_places=17)
-    latitude = models.DecimalField(max_digits=20, decimal_places=17)
+    title = models.CharField('Название', max_length=100)
+    short_description = models.TextField('Короткое описание', blank=True)
+    long_description = tinymce_models.HTMLField('Длинное опичание', blank=True)
+    longitude = models.DecimalField('Долгота', max_digits=20, decimal_places=17)
+    latitude = models.DecimalField('Широта', max_digits=20, decimal_places=17)
 
     class Meta:
         ordering = ['id']
@@ -20,10 +20,11 @@ class Image(models.Model):
     place = models.ForeignKey(
         Place,
         on_delete=models.CASCADE,
-        related_name='images'
+        related_name='images',
+        verbose_name="Место на карте"
     )
-    image = models.ImageField()
-    position = models.PositiveIntegerField(default=1)
+    image = models.ImageField('Картинка')
+    position = models.PositiveIntegerField('Позиция', default=1, db_index=True)
 
     class Meta:
         ordering = ['position']
